8eda79615ac010d66a02cad9aa5bf72f421e596a
Alex1um <ye4h@pm.me>
2025-03-22 17:25:29 +0700

    fixed usage

--- usage.py
+++ usage.py
@@ -1,21 +1,22 @@
 import stompws
 from dash import Dash, callback, html, Input, Output
+import json
 
 app = Dash(__name__)
 
 app.layout = html.Div([
     stompws.STOMPWebsocket(
         id='input',
-        value='my-value',
-        label='my-label'
+        url='ws://localhost:15674/ws',
+        subscribe="/topic/device.*.*",
     ),
     html.Div(id='output')
 ])
 
 
-@callback(Output('output', 'children'), Input('input', 'value'))
+@callback(Output('output', 'children'), Input('input', 'message'))
 def display_output(value):
-    return 'You have entered {}'.format(value)
+    return json.dumps(value)
 
 
 if __name__ == '__main__':
